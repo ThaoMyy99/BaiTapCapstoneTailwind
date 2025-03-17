663142b0b51815964cac8a0c37c5cee3ea7aeb40
ThaoMyy99 <hcthmy99@gmail.com>
2025-03-17 15:29:09 +0700

    chinh sua js

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,54 @@
+// scroll on counter start
+const counters = document.querySelectorAll('.counter');
 
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.5
+};
+
+const observer = new IntersectionObserver(handleIntersect, options);
+
+counters.forEach(counter => {
+    observer.observe(counter);
+});
+
+function handleIntersect(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            startCounting(entry.target);
+            observer.unobserve(entry.target);
+        }
+    });
+}
+
+function startCounting(element) {
+    const target = +element.getAttribute('data-target');
+    const increment = target / 200;
+
+    let count = 0;
+    const updateCounter = () => {
+        count += increment;
+        if (count < target) {
+            element.innerText = Math.ceil(count);
+            requestAnimationFrame(updateCounter);
+        } else {
+            element.innerText = target;
+        }
+    };
+
+    element.classList.add('visible');
+    updateCounter();
+}
+// scroll on counter end
+
+
+// scroll top top on button click start
+const scroll_up = document.getElementById("scroll-up")
+scroll_up.addEventListener("click", () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+})
+// scroll top top on button click end
 
 const testimonial = new Swiper('.testimonial-swiper', {
     autoplay: {
